e10df7340041383c8b389f52cf50d8528d3f295f
Joseph Kioko <kiokocodes@gmail.com>
2023-04-16 07:06:18 +0300

    if else test runs

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ function someRandomUpdate(){
 }
 
 
+/*function moo(y){
+    if(y < 0){
+        return "y is -negative or a floating point";
+    } else if (y > 0 && y < 1){
+        return "y is greater than zero and equal to 1";
+    } else if (y === 1 && y < 2 ){
+        return "y is equal to One or greater than 1"
+    }else {
+        return "y is grater than 2";
+    }
+}
+
+y = -0.2;
+console.log(moo(y));
+*/
